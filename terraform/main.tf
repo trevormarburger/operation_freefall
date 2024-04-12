f27865b929b5ee8a15e60f7c5fab8dbcaee5b82c
Trevor Marburger <trevormarburger@gmail.com>
2024-04-12 09:57:42 -0400

    Test secrets mgr pull.

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,12 +58,12 @@ resource "google_cloudfunctions_function" "function" {
   }
 }
 
-# IAM entry for a single user to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.function.project
-  region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
+# # IAM entry for a single user to invoke the function
+# resource "google_cloudfunctions_function_iam_member" "invoker" {
+#   project        = google_cloudfunctions_function.function.project
+#   region         = google_cloudfunctions_function.function.region
+#   cloud_function = google_cloudfunctions_function.function.name
 
-  role   = "roles/cloudfunctions.invoker"
-  member = "user:myFunctionInvoker@example.com"
-}
+#   role   = "roles/cloudfunctions.invoker"
+#   member = "user:myFunctionInvoker@example.com"
+# }
