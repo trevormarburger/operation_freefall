f77313d317ea36bb6623e27ff8a7efb8cd4228e1
Trevor Marburger <trevormarburger@gmail.com>
2024-04-12 12:04:19 -0400

    Add changing ts to func.

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "google_cloud_scheduler_job" "my_scheduler_job" {
 
 resource "google_cloudfunctions_function" "function" {
   name        = "operation-freefall-${var.env}"
-  description = "Operation Freefall Function"
+  description = "Operation Freefall Function - ${time_static.activation_date.unix}"
   runtime     = "python39"
 
   available_memory_mb          = 512
